53e56b99555c82921899434e658710ab448f58ca
Zeyad Tamimi <zeyadtamimi@outlook.com>
2018-11-06 21:34:50 -0800

    FN prototype

--- kaos/algorithm/__init__.py
+++ kaos/algorithm/__init__.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2018 Zeyad Tamimi.  All rights reserved.
-
 from collections import namedtuple
 
-Vector3D = namedtuple( "Vector3D", "x,y,z" )
-
+Vector3D = namedtuple("Vector3D", "x,y,z")
---
+++ kaos/algorithm/satellite.py
@@ -0,0 +1,9 @@
+from . import Vector3D
+
+def get_sattlite_pos_vel( satillite_info, time )
+    """  Satellite info is a number to be indexed from the DB.
+        time is in posix time.
+        needs to return ( Vector3D, Vector3D ) <- ( pos, vel ) at time
+    """
+    pass
+
