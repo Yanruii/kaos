8d520e2e3572a11fba74652c7a9e0112f8165f37
yanruii <yanrayli@gmail.com>
2019-02-18 00:46:51 -0800

    implement the brute force method on visibility finder.

--- test/algorithm/test_visibility_finder.py
+++ test/algorithm/test_visibility_finder.py
@@ -8,6 +8,7 @@ from kaos.algorithm.visibility_finder import VisibilityFinder
 from kaos.models import Satellite
 from kaos.models.parser import parse_ephemeris_file
 from kaos.utils.time_conversion import utc_to_unix
+import mpmath as mp
 
 from .. import KaosTestCase
 
@@ -117,3 +118,93 @@ class TestVisibilityFinder(KaosTestCase):
             if not found:
                 raise Exception('Wrong access: {}'.format(predicted_access))
 
+    @data(('test/algorithm/vancouver.test', (1514764802, 1514772000), 60),
+          ('test/algorithm/vancouver.test', (1514768543, 1514772000), 60),
+          ('test/algorithm/vancouver.test', (1514768340, 1514768400), 60),
+          ('test/algorithm/vancouver.test', (1514768543, 1514769143), 60),
+          ('test/algorithm/vancouver.test', (1515160800, 1515164400), 60))
+    def test_brute_force(self, test_data, time_step=60):
+        """Tests that the visibility finder produces the same results as the access file.
+           This is a brute force method. The self-adapting part is dropped. The root finding part
+           is dropped. This method checks the intersection between the Visibility curve and
+           sin(minimum_elevation_angle) and updates the visibility intervals.
+
+        Args:
+            test_data (tuple): A three tuple containing the:
+                                1 - The path of KAOS access test file
+                                2 - A tuple of the desired test duration
+                                3 - The maximum tolerated deviation in seconds
+            time_step (int)
+
+        Note:
+            Ranges provided must not start or end at an access boundary. This is a result
+            of the strict checking method provided.
+        """
+
+        mp.dps = 100
+        access_file, interval, max_error = test_data
+
+        access_info = self.parse_access_file(access_file)
+        finder = VisibilityFinder(Satellite.get_by_name(access_info.sat_name)[0].platform_id,
+                                  access_info.target, interval)
+
+        access_times = []
+        subinterval_start = interval[0]
+        subinterval_end = subinterval_start + time_step
+
+        if mp.mpf(finder.visibility(subinterval_start)) > 0:
+            visibility_last_sign = 1
+            start_time = interval[0]
+        else:
+            visibility_last_sign = 0
+
+        while subinterval_end < interval[1]:
+            visibility = mp.mpf(finder.visibility(subinterval_start))
+            # going from invisible to visible
+            if visibility > 0 and visibility_last_sign == 0:
+                start_time = subinterval_start
+            # going from visible to invisible
+            if visibility < 0 and visibility_last_sign == 1:
+                access_times.append((start_time, subinterval_start))
+            # ending the checking
+            if (subinterval_end + time_step) > interval[1]:
+                if not access_times:
+                    access_times.append((start_time, interval[1]))
+                elif start_time != access_times[len(access_times)-1][0]:
+                    access_times.append((start_time, interval[1]))
+            if visibility > 0:
+                visibility_last_sign = 1
+            if visibility < 0:
+                visibility_last_sign = 0
+            subinterval_start = subinterval_end
+            subinterval_end += time_step
+        #deal with the case where the poi is less than the step time:
+        if len(access_times) == 0 and finder.visibility(subinterval_start) > 0:
+            access_times.append((interval[0], interval[1]))
+
+        for predicted_access in access_times:
+            found = False
+            for actual_access in access_info.accesses:
+                if (interval[0] > actual_access[0]) and (interval[1] < actual_access[1]):
+                    if ((abs(interval[0] - predicted_access[0]) < max_error) and
+                            (abs(interval[1] - predicted_access[1]) < max_error)):
+                        found = True
+                        break
+                if interval[0] > actual_access[0]:
+                    if ((abs(interval[0] - predicted_access[0]) < max_error) and
+                            (abs(actual_access[1] - predicted_access[1]) < max_error)):
+                        found = True
+                        break
+                elif interval[1] < actual_access[1]:
+                    if ((abs(actual_access[0] - predicted_access[0]) < max_error) and
+                            (abs(interval[1] - predicted_access[1]) < max_error)):
+                        found = True
+                        break
+
+                if ((abs(actual_access[0] - predicted_access[0]) < max_error) and
+                        (abs(actual_access[1] - predicted_access[1]) < max_error)):
+                    found = True
+                    break
+
+            if not found:
+                raise Exception('Wrong access: {}'.format(predicted_access))
